2e567fc0192251ccde67ac8d9a0fadcb21d20f6a
kalpitk <kkothari1999@gmail.com>
2019-02-09 19:48:02 +0530

    update map.js

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import sqlite3
-
+import mysql.connector
 from flask import (Flask, g, redirect, render_template, request, session,
                    url_for)
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host='localhost',
+  user='root',
+  password='',
+  database = 'PROJECT'
+)
+cursor = mydb.cursor(buffered=True)
 
 @app.route("/home", methods = ['POST', 'GET'])
 def home():
--- static/js/map.js
+++ static/js/map.js
@@ -1,38 +1,43 @@
-var coordinates = new Array();
-coordinates.push([{lat: -34.397, lng: 150.644},{lat: -33.397, lng: 149.644}]);
+/* Add this to HTML template
+ * <script> const coordinates = {{res|tojson|safe}}; </script>
+ * where res stores the co-ordinates selected from db
+ */
 var map;
 function addMarker(coord) {
     var marker1 = new google.maps.Marker({
-        position: coord[0],
+        position: {lat:coord[0], lng:coord[1]},
         map:map
     });
     var marker2 = new google.maps.Marker({
-        position: coord[1],
+        position: {lat:coord[2], lng:coord[3]},
         map:map
     });
     var path = new google.maps.Polyline({
         map: map,
-        path: coord,
+        path: [{lat:coord[0], lng:coord[1]}, {lat:coord[2], lng:coord[3]}],
         geodesic: false,
         strokeColor: 'red',
         strokeOpacity: 1.0,
         strokeWeight: 5
-    })
+    });
     path.addListener('click', function () {
         alert('path clicked');
-    })
+    });
     marker1.addListener('click', function () {
         alert('source clicked');
-    })
+    });
     marker2.addListener('click', function () {
         alert('destination clicked');
-    })
-    console.log('DONE');
+    });
 }
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
     });
-    addMarker(coordinates[0]);
+    const len = coordinates.length;
+    console.log(len);
+    for(var i=0;i<len;i++) {
+        addMarker(coordinates[i]);
+    }
 }
